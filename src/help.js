2d63dfacb2a228f1cb3e9ed18fe92a64756d283c
Jirenw <89024238+Jirenw@users.noreply.github.com>
2021-08-16 19:29:05 -0500

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,7 +1,8 @@
 const help = (prefix) => {
 	return `
 
-⌜ *BoT-☪⃟⚜⃝𝔸ℕ𝕀𝕄𝔼&𝕄𝔸𝕊⃝➳⃟♾<>By.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽* ⌟  
+「*BoT-☪⃟⚜⃝𝔸ℕ𝕀𝕄𝔼&𝕄𝔸𝕊⃝➳⃟♾*」
+「*By.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽* ⌟  
 
 ◉ *INFORMACION*
    ○ Comando: ⌜ ${prefix} ⌟
@@ -11,91 +12,97 @@ const help = (prefix) => {
 
 
 ◉ *NUEVOS COMANDOS*
-○ ${prefix}levelmenu
-Level para tus grupos.
-○ ${prefix}antimenu
-Su nombre lo dice todo (antilink).
-○ ${prefix}otak
-Monas chinas.
-○ ${prefix}shantera
-Interactua con el bot.
+  ║
+  ╠○ ${prefix}levelmenu
+  ╠ ➢Level para tus grupos.
+  ╠○ ${prefix}antimenu
+  ╠ ➢Su nombre lo dice todo (antilink).
+  ╠○ ${prefix}otak
+  ╠ ➢Monas chinas.
+  ╠○ ${prefix}shantera
+  ╚ ➢Interactua con el bot.
 
 ◉ *NUEVOS MENUS*
-○ ${prefix}juegos
-Divierte con tus amigos :)
-○ ${prefix}desmenu
-Descargar musica y videos de YT.
-○ ${prefix}version
-Conoce la versión de tu bot.
-○ ${prefix}welmenu
-Comandos de bienvedia a grupos.
+  ║
+  ╠○ ${prefix}juegos
+  ╠ ➢Divierte con tus amigos :)
+  ╠○ ${prefix}desmenu
+  ╠ ➢Descargar musica y videos de YT.
+  ╠○ ${prefix}version
+  ╠ ➢Conoce la versión de tu bot.
+  ╠○ ${prefix}welmenu
+  ╚ ➢Comandos de bienvedia a grupos.
 
 ◉ *PARA USAR EL BOT*
 Registrate con el comando ${prefix}daftar y tu nombre
 
 ◉ *CREAR STICKERS*
   ║
-  ╠ ○ ${prefix}sticker
-  ╠ ○ ${prefix}attp
-  ╠ Mas un texto corto
-  ╠ ○ ${prefix}stickergif
-  ╚ 6 segundos de video
+  ╠○ ${prefix}sticker
+  ╠○ ${prefix}attp
+  ╠ ➢Mas un texto corto
+  ╠○ ${prefix}stickergif
+  ╚ ➢6 segundos de video
 
 ◉ *CONVERTIDORES*
   ║
-  ╠ ○ ${prefix}toimg
-  ╠ De sticker a JPG
-  ╠ ○ ${prefix}tomp3
-  ╚ De video a MP3
+  ╠○ ${prefix}toimg
+  ╠ ➢De sticker a JPG
+  ╠○ ${prefix}tomp3
+  ╚ ➢De video a MP3
 
 ◉ *AUDIO*
   ║
-  ╠ ○ ${prefix}idioma
-  ╚ ○ ${prefix}tts es (mas texto)
+  ╠○ ${prefix}idioma
+  ╠○ ${prefix}tts es
+  ╚ ➢mas texto)
 
 ◉ *OTROS*
   ║
-  ╠ ○ ${prefix}wame
-  ╠ Link de Whatsapp
-  ╠ ○ ${prefix}qrcode
-  ╚ Coloca un texto
+  ╠○ ${prefix}wame
+  ╠ ➢Link de Whatsapp
+  ╠○ ${prefix}qrcode
+  ╚ ➢Coloca un texto
 
 ◉ *GRUPOS*
   ║
-  ╠ ○ ${prefix}closegc
-  ╠ Cerrar el grupo solo admins
-  ╠ ○ ${prefix}opengc
-  ╠ Abrir grupo solo admins
-  ╠ ○ ${prefix}kickmenu
-  ╠ Eliminar a un miembro 
-  ╠ ○ ${prefix}promote
-  ╠ Dar admin a un miembro
-  ╠ ○ ${prefix}demote
-  ╠ Quitar el admin
-  ╠ ○ ${prefix}linkgc
-  ╠ Link del grupo
-  ╠ ○ ${prefix}adminlist
-  ╠ Nombra a los admins del grupo
-  ╠ ○ ${prefix}todos
-  ╚ Nombra a todos los del grupo
+  ╠○ ${prefix}closegc
+  ╠ ➢Cerrar el grupo solo admins
+  ╠○ ${prefix}opengc
+  ╠ ➢Abrir grupo solo admins
+  ╠○ ${prefix}kickmenu
+  ╠ ➢Eliminar a un miembro 
+  ╠○ ${prefix}promote
+  ╠ ➢Dar admin a un miembro
+  ╠○ ${prefix}demote
+  ╠ ➢Quitar el admin
+  ╠○ ${prefix}linkgc
+  ╠ ➢Link del grupo
+  ╠○ ${prefix}adminlist
+  ╠ ➢Nombra a los admins del grupo
+  ╠○ ${prefix}todos
+  ╚ ➢Nombra a todos los del grupo
 
 Para usar estas funciones el bot necesita admin
   
 ◉ *NSWF* 
   ║
-  ╚ ○ ${prefix}nsfwmenu
+  ╚○ ${prefix}nsfwmenu
 
 Para activar los NSFW coloque el siguiente comando ${prefix}nsfw 1 y para desactivar los NSFW coloque ${prefix}nsfw 0
 
 NOTAS.
 
-No Hagas spam ctmr, si no responde rápido espera. ;)
+➢No Hagas spam ctmr, si no responde rápido espera. ;)
 
-No entres al grupo ofc a enviar tu enlace no seas Gil(a).
+➢No entres al grupo ofc a enviar tu enlace no seas Gil(a).
+
+➢Si quieres que el bot esté en tu grupo no hay problema manda mensaje al pv de Jiren :v.
+La única condición es que el bot enviara el enlace dem grupo ofc, ya que está incrustados en el menú.
 
 *By.〲⎠⎠ɪʀᴇɴ〴᭄Y🌹⃢⃟ꦼ𝔚𝔢𝔫𝔡𝔶-𝔠𝔥𝔞𝔫༒⃢⃟🇲🇽*
 
-⌜ *🔥♥️Te Amo Wendy♥️🔥, Eres mi complemento Perfecto, Llegaste de la nada y te convertiste en mi tod👩‍❤️‍👨* ⌟ 
+⌜ *🔥♥️Te Amo Wendy♥️🔥, Eres mi complemento Perfecto, Llegaste de la nada y te convertiste en mi todo👩‍❤️‍👨* ⌟ 
 `
 }
 
